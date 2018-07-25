01d9e4aa0b391b04c30662c1b8e46d5475c8feca
Hakan Ilter <hakan.ilter@argos.co.uk>
2018-07-25 09:42:33 +0100

    Added database-health.sh script

---
+++ scripts/database-health.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+source ./environment.sh
+
+DATABASE=$1
+if [ -z "$1" ]
+  then
+    echo "Please provide a database name as an argument"
+    exit
+fi
+
+time java $JAVA_OPTS -cp $JAR_FILE com.devveri.hive.tool.DatabaseHealthTool "$IMPALA_SERVER" $DATABASE
