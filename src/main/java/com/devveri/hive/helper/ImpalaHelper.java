c51275fdf387609d2f75c5b51dfbfbcc022b5ec5
Hakan Ilter <hakan.ilter@argos.co.uk>
2018-08-22 13:47:45 +0100

    Added refresh table command before metadata read

--- src/main/java/com/devveri/hive/helper/ImpalaHelper.java
+++ src/main/java/com/devveri/hive/helper/ImpalaHelper.java
@@ -23,33 +23,39 @@ public class ImpalaHelper extends HiveHelper {
         long start = System.currentTimeMillis();
         List<PartitionMetadata> list = new ArrayList<>();
 
-        final String query = String.format("SHOW PARTITIONS %s.%s", database, table);
-        try (Connection con = getConnection(config.getUrl());
-             PreparedStatement stmt = con.prepareStatement(query);
-             ResultSet rs = stmt.executeQuery()) {
-            while (rs.next()) {
-                ResultSetMetaData metaData = rs.getMetaData();
-                PartitionMetadata partitionMetadata = new PartitionMetadata();
-                // fill columns
-                for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
-                    if (!RESERVED_COLUMN_NAMES.contains(metaData.getColumnName(i))) {
-                        Object value = rs.getObject(i);
-                        partitionMetadata.getPartitionColumns().put(metaData.getColumnName(i),
-                                value instanceof String && "NULL".equalsIgnoreCase((String) value) ? null : value);
+        try (Connection con = getConnection(config.getUrl())) {
+            // refresh table first
+            try (Statement stmt = con.createStatement()) {
+                stmt.execute(String.format("REFRESH %s.%s", database, table));
+            }
+            // get metadata
+            try (PreparedStatement stmt = con.prepareStatement(String.format("SHOW PARTITIONS %s.%s", database, table));
+                 ResultSet rs = stmt.executeQuery()) {
+                while (rs.next()) {
+                    ResultSetMetaData metaData = rs.getMetaData();
+                    PartitionMetadata partitionMetadata = new PartitionMetadata();
+                    // fill columns
+                    for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
+                        if (!RESERVED_COLUMN_NAMES.contains(metaData.getColumnName(i))) {
+                            Object value = rs.getObject(i);
+                            partitionMetadata.getPartitionColumns().put(metaData.getColumnName(i),
+                                    value instanceof String && "NULL".equalsIgnoreCase((String) value) ? null : value);
+                        }
                     }
+                    partitionMetadata.setRows(rs.getInt("#Rows"));
+                    partitionMetadata.setFiles(rs.getInt("#Files"));
+                    partitionMetadata.setSize(HdfsUtil.getFileSizeFromHumanReadable(rs.getString("Size")));
+                    partitionMetadata.setFormat(rs.getString("Format"));
+                    partitionMetadata.setIncrementalStats(Boolean.parseBoolean(rs.getString("Incremental stats")));
+                    partitionMetadata.setLocation(rs.getString("Location"));
+                    list.add(partitionMetadata);
                 }
-                partitionMetadata.setRows(rs.getInt("#Rows"));
-                partitionMetadata.setFiles(rs.getInt("#Files"));
-                partitionMetadata.setSize(HdfsUtil.getFileSizeFromHumanReadable(rs.getString("Size")));
-                partitionMetadata.setFormat(rs.getString("Format"));
-                partitionMetadata.setIncrementalStats(Boolean.parseBoolean(rs.getString("Incremental stats")));
-                partitionMetadata.setLocation(rs.getString("Location"));
-                list.add(partitionMetadata);
             }
         }
 
         long end = System.currentTimeMillis();
-        logger.info("Query \"{}\" execution took {} ms", query, (end-start));
+        logger.info("Query \"{}\" execution took {} ms", String.format("REFRESH %s.%s; SHOW PARTITIONS %s.%s",
+                database, table, database, table), (end-start));
 
         // last record is summary record, ignore it
         return list.size() > 0 ? list.subList(0, list.size() - 1) : list;
