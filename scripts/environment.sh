2c64f72a5b06c250be9acaa7d2f99b6da6884de5
Hakan Ilter <hakan.ilter@argos.co.uk>
2018-09-05 14:04:27 +0100

    Updated environment.sh

--- scripts/environment.sh
+++ scripts/environment.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-export HIVE_SERVER=localhost:10000
-export IMPALA_SERVER=localhost:21050
+export HIVE_SERVER="jdbc:hive2://quickstart.cloudera:10000/"
+export IMPALA_SERVER="jdbc:hive2://quickstart.cloudera:21050/;auth=noSasl"
 export JAVA_OPTS="" # add "-Dhadoop.security.authentication=kerberos" for kerberos auth
 export JAR_FILE="lib/devveri-hive-tools-0.0.1-SNAPSHOT-dist.jar"
